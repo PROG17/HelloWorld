56f0d2108d6225891eab726f2cac09971f7508fa
Andreas <antenilsson87@hotmail.com>
2017-09-11 12:22:54 +0200

    Andreas was here

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -11,6 +11,8 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Andreas was here!");
         }
     }
 }
